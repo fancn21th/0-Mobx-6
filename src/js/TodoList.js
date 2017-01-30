442eea801d58b7174a9c6d53bb4df021b8606695
fancn <fanyj@yikeer.com>
2017-01-30 13:28:53 +0800

    first commit

---
+++ src/js/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class TodoList extends React.Component {
+    render(){
+        return <h1>Hello MobX</h1>
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import "../css/main.css"
+import React from "react"
+import ReactDOM from "react-dom"
+
+import TodoList from "./TodoList"
+
+const app = document.getElementById("app")
+
+ReactDOM.render(<TodoList/>, app)
+
